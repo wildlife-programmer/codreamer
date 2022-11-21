91cb1b91553f3fc94e29d5ac388b53144494d0db
wildlife-programmer <wilelife-programmer1000@gmail.com>
2022-11-22 03:10:14 +0900

    방명록 스토리지 생성

--- codreamer-server/data/modules/rpc.lua
+++ codreamer-server/data/modules/rpc.lua
@@ -5,7 +5,11 @@ local jsonDecode = nk.json_decode
 local matchList = nk.match_list
 local matchCreate = nk.match_create
 
+local storageRead = nk.storage_read
+local storageWrite = nk.storage_write
+
 local registerRpc = nk.register_rpc
+local runOnce = nk.run_once
 
 local function match_join(context, payload)
     local packed = {success = true}
@@ -26,13 +30,30 @@ local function match_create(context, payload)
         local module_name = "master"
         match_id = matchCreate(module_name)
     else
-        print("@@@@ searched match", jsonEncode(matches[1]))
         match_id = matches[1].match_id
     end
 
     return match_id
 end
 
+local function create_storage()
+    local config_1 = {{collection = "guestbook", key = "guestbook"}}
+    local objects_1 = storageRead(config_1);
+    if #objects_1 <= 0 then
+        config_1[1].value = {comments = {}}
+        storageWrite(config_1)
+    end
+end
+
+local function get_guestbook()
+    local config_1 = {{collection = "guestbook", key = "guestbook"}}
+    local objects_1 = storageRead(config_1);
+    if #objects_1 > 0 then return jsonEncode(objects_1[1].value) end
+end
+
+runOnce(create_storage)
+
 registerRpc(match_join, "match_join")
 registerRpc(match_create, "match_create")
+registerRpc(get_guestbook, "get_guestbook")
 
